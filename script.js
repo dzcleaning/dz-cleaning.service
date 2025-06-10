c8ef6bbddb121f2bc24d13bc69c7a403e69c5a9a
dzcleaning <dz.cleaning@yahoo.com>
2025-06-10 07:02:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function switchLang(lang) {
+  document.querySelectorAll('[data-lang]').forEach(e => e.classList.remove('visible'));
+  document.querySelectorAll('[data-lang="' + lang + '"]').forEach(e => e.classList.add('visible'));
+}
+window.onload = () => switchLang('ar');
 
