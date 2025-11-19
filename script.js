852a520f4d2937a80014baf36607097d45f918b7
dzcleaning <dz.cleaning@yahoo.com>
2025-11-19 20:45:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+<script>
+  function switchLang(lang) {
+    document.querySelectorAll('[data-lang]').forEach(el => {
+      el.style.display = el.getAttribute('data-lang') === lang ? 'block' : 'none';
+    });
+    localStorage.setItem('lang', lang);
+  }
 
+  window.onload = () => {
+    const savedLang = localStorage.getItem('lang') || 'fr';
+    switchLang(savedLang);
+  };
+</script>
