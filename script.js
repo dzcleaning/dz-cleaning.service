effb4cac2fc44d494034ff80f40396780e3b6270
dzcleaning <dz.cleaning@yahoo.com>
2025-11-19 20:27:42 +0100

    Remove language switching functionality
    Remove language switching script from script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-<script>
-  function switchLang(lang) {
-    document.querySelectorAll('[data-lang]').forEach(el => {
-      el.style.display = el.getAttribute('data-lang') === lang ? 'block' : 'none';
-    });
-    localStorage.setItem('lang', lang);
-  }
 
-  window.onload = () => {
-    const savedLang = localStorage.getItem('lang') || 'fr';
-    switchLang(savedLang);
-  };
-</script>
